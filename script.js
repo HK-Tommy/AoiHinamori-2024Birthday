f194958acb4df49585580cc9642ee4b21c4ac633
HK 湯米Tommy <tlamhappy10@gmail.com>
2024-09-07 00:34:01 +0800

    Update

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function GetParams() {
     const urlParams = new URLSearchParams(window.location.search);
     const pages = urlParams.get('pages');
+    const redirect_url = urlParams.get('redirect');
   
     if (pages === "1") {
         document.getElementById('pages2').style.display = 'none'
@@ -23,6 +24,12 @@ function GetParams() {
     }
     else if (pages === "download") {
         window.open('https://1drv.ms/u/s!AmxjBGhGag2mhI0cVE2fRzpNGlRcww?e=EvimS2', '_blank');
+        document.getElementById('pages1').style.display = 'none'
+        document.getElementById('pages2').style.display = 'none'
+        document.getElementById('pages3').style.display = 'none'
+        document.getElementById('contact').style.display = 'none'
+        document.getElementById('download').style.display = ''
+        
     }
     else if (pages === "contact") {
         document.getElementById('pages1').style.display = 'none'
@@ -30,9 +37,17 @@ function GetParams() {
         document.getElementById('pages3').style.display = 'none'
         document.getElementById('contact').style.display = ''
     }
+
+    if (redirect_url != null) {
+        redirectpage(redirect_url)
+    }
 }
 GetParams();
 
 if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone/i)) {
     window.location.replace('error.html');
+}
+
+function redirectpage(url) {
+    window.location.replace(url);
 }
